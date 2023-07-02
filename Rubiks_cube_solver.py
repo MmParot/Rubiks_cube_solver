f099a4a41c555a8fecbf1bb50fe9f128953b8e2b
MmParot <136229364+MmParot@users.noreply.github.com>
2023-07-02 17:27:27 +1200

    Added setlist of ideas, made the variable notation, added a section where all the variables are kept, aded blue as a   list, imported random, aded notes about the general idea of this code

--- Rubiks_cube_solver.py
+++ Rubiks_cube_solver.py
@@ -1,3 +1,18 @@
 #this is the idea section
+#Add a number for each colour eg: 1 = red, 2 = blue etc.
+#we could just muddle up the numbers but then 90% of the time the cube/position wouold be unsolvable.
+#make a list  noting each side accordingly and then take 012(list figures)from the list and move
+#them to the next.
+#
+#
+#
+
+#this is the upload section
+
+import random
+#this is the section where all the variables are set
+
+Notation = ["l", "r", "d", "u", "l'", "r'", "d'", "u'", "s", "m", "e", "s'", "m'", "e'"]
+blue = ["0", "0", "0", "0", "0", "0", "0", "0", "0"]
 
 #This is the code seccction
