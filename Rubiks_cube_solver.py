7c7143e0cb9c4ea6536b4a98e905f39508baca9c
MmParot <136229364+MmParot@users.noreply.github.com>
2024-09-27 10:59:00 +1200

    Updated Rubiks_cube_solver.py
    Added a For add in Notation.
    Added more normal Notation for less double turns

--- Rubiks_cube_solver.py
+++ Rubiks_cube_solver.py
@@ -14,7 +14,7 @@ import random
 
 time = int(input("how many move scramble would you like?   "))
 
-Notation = ["U", "D", "L", "R", "U'", "D'", "L'", "R'", "U2", "D2", "L2", "R2", "U2'", "D2'", "L2'", "R2'"]
+Notation = ["U", "U", "D", "L", "R", "U'", "D' ", "L' ", "R'", "D", "L", "R", "U'", "D'", "L'", "R'", "U2", "D2", "L2", "R2", "U2'", "D2'", "L2'", "R2'"]
 
 white = ["0", "0", "0", "0", "0", "0", "0", "0", "0"]
 yellow = ["1", "1", "1", "1", "1", "1", "1", "1", "1"]
@@ -24,16 +24,17 @@ red = ["4", "4", "4", "4", "4", "4", "4", "4", "4"]
 orange = ["5", "5", "5", "5", "5", "5", "5", "5", "5"]
 
 scramble = []
+Scramble_check_time = 0
 
 #This is the code section
 def scramble_notation(time,scramble):
     while  time >0:
-        t = random.choice(Notation)
-        scramble.append(t)
-#       print(time)
-        time = time - 1
+        add = random.choice(Notation)
+        for add in Notation:
+            
+            scramble.append(add)
+    #       print(time)
+            time = time - 1
     print(scramble)
     
 scramble_notation(time, scramble = [],  )
-    
-    
